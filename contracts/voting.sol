57983a702a0f4147a33da944d413e8a30d17a853
HiranSaravanan <hiranvikraman2001sr@gmail.com>
2022-08-22 22:01:45 +0530

    Contract Setup

---
+++ contracts/voting.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract voting{
+    string public candidate;
+    constructor() public
+    {
+        candidate = "Candidate 1";
+    }
+
+}
